8d7d485d959890e710e73722c4382fcecc8b35bd
sNiGdHa111 <69614861+sNiGdHa111@users.noreply.github.com>
2023-10-02 17:21:33 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,51 @@
+import random
+str(input("Do you want to roll the dice? Input Y or N"))
+while dice(response==y):
+    randint(start,end)
+    print(no)
+no = random.randint(1,6)
+
+if dice(response==n):
+    print("You chose not roll the dice")
+    
+if no == 1:
+    print("[-----]")
+    print("[     ]")
+    print("[  1  ]")
+    print("[     ]")
+    print("[-----]")
+    
+if no == 2:
+    print("[-----]")
+    print("[     ]")
+    print("[  2  ]")
+    print("[     ]")
+    print("[-----]")
+    
+if no == 3:
+    print("[-----]")
+    print("[     ]")
+    print("[  3  ]")
+    print("[     ]")
+    print("[-----]")
+    
+if no == 4:
+    print("[-----]")
+    print("[     ]")
+    print("[  4  ]")
+    print("[     ]")
+    print("[-----]")
+    
+if no == 5:
+    print("[-----]")
+    print("[     ]")
+    print("[  5  ]")
+    print("[     ]")
+    print("[-----]")
+    
+if no == 6:
+    print("[-----]")
+    print("[     ]")
+    print("[  6  ]")
+    print("[     ]")
+    print("[-----]")
